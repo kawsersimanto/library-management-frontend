747764e45a25ec557ef8d4cd45351a95b44d1739
kawsersimanto <kawsersimanto@gmail.com>
2025-07-03 17:59:07 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/ui/mode-toggle";
+
+const App = () => {
+  return (
+    <div>
+      <ModeToggle />
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
